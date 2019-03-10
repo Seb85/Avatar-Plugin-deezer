9b9df2e11ea3d752820983738508d87ce0002573
Seb85 <sebrenaud@hotmail.fr>
2019-03-10 12:13:13 +0100

    Add files via upload

---
+++ intent.deezer.js
@@ -0,0 +1,25 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _helpers = require('../../node_modules/ava-ia/lib/helpers');
+
+// -- Internal
+//var RULES = ['translate [Preposition]? [Demonym]', 'translate * [Preposition] [Demonym]'];
+
+exports.default = function (state, actions) {
+	
+	if (state.isIntent) return (0, _helpers.resolve)(state);
+	
+	var tokens = (0, _helpers.intersect)(Config.modules.deezer.rules.deezer, state.tokens);
+
+	if (tokens) {
+		if (state.debug) info('Intentdeezer'.bold.green, 'syntax:', 'true'.green );
+		state.isIntent = true;
+		return (0, _helpers.factoryActions)(state, actions);
+	} else 
+		return (0, _helpers.resolve)(state);	
+  
+};
